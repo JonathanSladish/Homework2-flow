810b3e0af725d7401cd2a13927132934276854c7
JonathanSladish <75808482+JonathanSladish@users.noreply.github.com>
2021-01-25 22:17:16 -0600

    Create AsciiChar.java

---
+++ AsciiChar.java
@@ -0,0 +1,26 @@
+package com.company;
+
+
+public class AsciiChars {
+    public static void main(String[] args) {
+        printNumber();
+        printLowerCase();
+        printUpperCase();
+    }
+    public static void printNumbers() {
+        for (int i-(int)'0'; i<=(int)'9'; i++) {
+            System.out.printf("%c is %d\n", (char)i, i);
+        }
+    }
+    public static void printLowerCase() {
+        for (int i=(int)'a'; i<=(int)'z'; i++)
+        {
+            System.out.printf(%c is %d\n", (char)i, i);
+        }
+    }
+    public static void printUpperCase() {
+        for (int i=(int)'A'; i<=(int)'Z'; i++) {
+            System.out.printf("%c is %d\n", (char)i, i);
+        }
+    }
+
